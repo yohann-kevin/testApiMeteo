4e4b186546ea97d77c99177bd8c9598d4d94746f
Perriguey <yo44prg@icloud.com>
2021-01-12 17:58:29 +0100

    feat : modify function searchCity

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ function init() {
     let input = document.createElement("input");
     input.type = "text";
     input.id = "inputCity"
-    
+
     let btn = document.createElement("button");
     btn.textContent = "Search";
     btn.id = "btnSearchCity";
@@ -29,11 +29,11 @@ function init() {
     body.appendChild(input);
     body.appendChild(btn);
     body.appendChild(div);
-    detectSearch(input,btn);
+    detectSearch(input, btn);
 }
 
-function detectSearch(input,btn) {
-    input.addEventListener("keyup", function() {
+function detectSearch(input, btn) {
+    input.addEventListener("keyup", function () {
         let cityRead = input.value;
         searchCity(cityRead);
     })
@@ -47,18 +47,21 @@ function searchCity(cityRead) {
     fetch(link + token + "&search=" + cityRead).then(function (response) {
         return response.json();
     }).then(function (json) {
-        for (let i = 0; i < 3; i++) {
-            let p = document.createElement("p");
-            p.classList.add("list");
-            p.textContent = json.cities[i].name + ", " + json.cities[i].cp;
-            p.style.cursor = "pointer";
-            wrapper.appendChild(p);
-            p.addEventListener("click", function() {
-                document.getElementById("inputCity").value = json.cities[i].name;
-                wrapper.innerHTML = "";
-                searchMeteoApi();
-            })
+        if (cityRead.length > 3) {
+            for (let i = 0; i < 3; i++) {
+                let p = document.createElement("p");
+                p.classList.add("list");
+                p.textContent = json.cities[i].name + ", " + json.cities[i].cp;
+                p.style.cursor = "pointer";
+                wrapper.appendChild(p);
+                p.addEventListener("click", function () {
+                    document.getElementById("inputCity").value = json.cities[i].name;
+                    wrapper.innerHTML = "";
+                    searchMeteoApi();
+                })
+            }
         }
+
     })
 }
 
